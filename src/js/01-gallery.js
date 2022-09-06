3fba1bc615d32a42d9fb00f47d3a0b4b2cf0838f
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-06 14:49:42 +0300

    Готовое 1 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsListEl = galleryItems.map(el => {
+    const galleryLinkEl = document.createElement('a');
+    galleryLinkEl.classList.add('gallery__link');
+    galleryLinkEl.href = el.original;
+
+    const galleryImageEl = document.createElement('img');
+    galleryImageEl.classList.add('gallery__image');
+    galleryImageEl.src = el.preview;
+    galleryImageEl.alt = el.description;
+
+    galleryLinkEl.append(galleryImageEl);    
+
+    return  galleryLinkEl;
+})
+
+galleryEl.append(...galleryItemsListEl);
+console.log(galleryEl);
+
+const lightBox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
